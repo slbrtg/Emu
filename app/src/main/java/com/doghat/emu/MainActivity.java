92cccf30df4ab674c5c4c7f1693bf7bae4970cd1
Saul Artiga <slbrtg@gmail.com>
2017-09-08 15:47:08 -0700

    create displaying grid and button from template

---
+++ app/src/main/java/com/doghat/emu/MainActivity.java
@@ -0,0 +1,19 @@
+package com.doghat.emu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+    GridView gridView;
+    String[] letters = new String[]{"SNES","PSX","GBA"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridView = (GridView) findViewById(R.id.baseGridView);
+        gridView.setAdapter(new AlphabetAdapter(this, letters));
+    }
+}
