35b7ad983debdb0a9e9651b790cd5828fcbd4463
Saul Artiga <slbrtg@gmail.com>
2017-09-10 17:23:08 -0700

    add second activity for adding new emulators, integrate butterknife to mainactivity

--- app/src/main/java/com/doghat/emu/MainActivity.java
+++ app/src/main/java/com/doghat/emu/MainActivity.java
@@ -1,19 +1,34 @@
 package com.doghat.emu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     GridView gridView;
-    String[] emulators = new String[]{"gameboy","nes","psp"};
+    @Bind(R.id.addNewEmulatorButton) Button addNewEmulatorButton;
+    String[] emulators = new String[]{"gba","nes","psp", "genesis"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         gridView = (GridView) findViewById(R.id.baseGridView);
         gridView.setAdapter(new EmulatorGridAdapter(this, emulators));
+
+        addNewEmulatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewEmulator.class);
+                startActivity(intent);
+            }
+        });
     }
 }
